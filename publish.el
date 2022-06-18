733b7737b48068a1dd6ad8517baa5f70639908da
hugs <h.t.dictus@gmail.com>
2022-06-18 13:45:59 +0200

    add all current writing

---
+++ publish.el
@@ -0,0 +1,18 @@
+(require 'ox-publish)
+
+(setq org-publish-project-alist
+      '(
+
+("my-blog"
+ :base-directory "/home/hugs/Projects/writing/blog/"
+ :base-extension "org"
+ :publishing-directory "/home/hugs/Projects/writing/blog_published/"
+ :recursive t
+ :publishing-function org-html-publish-to-html
+ :headline-levels 4             ; Just the default for this project.
+ :auto-preamble t
+ )
+
+))
+(org-roam-update-org-id-locations)
+(org-publish-project "my-blog")

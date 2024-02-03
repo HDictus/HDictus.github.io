b41cc3fe7a5daa3c2ec81d3652d7ef2bf58aa815
hugs <h.t.dictus@gmail.com>
2024-02-03 12:26:28 +0100

    a few changes

--- publish.el
+++ publish.el
@@ -1,5 +1,20 @@
 (require 'ox-publish)
 
+(setq org-html-postamble
+"  <div id=\"comments\">
+    <h2>Comments</h2>
+    <div id=\"comment-list\"></div>
+    <div><textarea id=\"author\" placeholder=\"Name\"></textarea></div>
+
+    <div><textarea id=\"comment-input\" placeholder=\"Leave a comment...\"></textarea></div>
+    <button onclick=\"submitComment()\">Submit</button>
+  </div>
+
+  <script src=\"scripts/comments.js\"></script>
+"
+)
+
+
 (setq org-publish-project-alist
       '(
 
@@ -11,8 +26,16 @@
  :publishing-function org-html-publish-to-html
  :headline-levels 4             ; Just the default for this project.
  :auto-preamble t
+ :postamble org-export-html-postamble
  )
-
+("blog-attachments"
+ :base-directory "/home/hugs/Dropbox/writing/blog/"
+ :base-extension "js//|png//|json"
+ :publishing-directory "/home/hugs/Dropbox/writing/blog_published/"
+ :recursive t 
+ :publishing-function org-publish-attachment
+ )
+("blog" :components ("my-blog" "blog-attachments"))
 ))
 (org-roam-update-org-id-locations)
-(org-publish-project "my-blog")
+(org-publish-project "blog")

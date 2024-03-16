9d898f332357164e9922933e5eb5d194c8ebbefd
hugs <h.t.dictus@gmail.com>
2024-03-16 19:21:38 +0100

    Structure blog in git compatible way

--- publish.el
+++ publish.el
@@ -5,23 +5,16 @@
       '(
 
 ("my-blog"
- :base-directory "/home/hugs/Dropbox/writing/blog/"
+ :base-directory "/home/hugs/Dropbox/writing/blog/org-files"
  :base-extension "org"
- :publishing-directory "/home/hugs/Dropbox/writing/blog_published/"
+ :publishing-directory "/home/hugs/Dropbox/writing/blog/"
  :recursive t
  :publishing-function org-html-publish-to-html
  :headline-levels 4             ; Just the default for this project.
  :auto-preamble t
  :postamble org-export-html-postamble
  )
-("blog-attachments"
- :base-directory "/home/hugs/Dropbox/writing/blog/"
- :base-extension "js//|png//|json"
- :publishing-directory "/home/hugs/Dropbox/writing/blog_published/"
- :recursive t 
- :publishing-function org-publish-attachment
- )
-("blog" :components ("my-blog" "blog-attachments"))
+("blog" :components ("my-blog"))
 ))
 (org-roam-update-org-id-locations)
 (org-publish-project "blog")

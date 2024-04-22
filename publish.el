9478e04acb05a1fc85582d878b0c197ae4f56911
hugs <h.t.dictus@gmail.com>
2024-04-22 18:47:55 +0200

    Add subscribe box

--- publish.el
+++ publish.el
@@ -1,8 +1,20 @@
 (require 'ox-publish)
 
+(setq org-html-preamble
+    "<!-- MailerLite Universal -->
+<script>
+    (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+    .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+    n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+    (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+    ml('account', '919123');
+</script>
+<!-- End MailerLite Universal -->"
+)
 
 (setq org-html-postamble
-      "<script src=\"https://utteranc.es/client.js\"
+      "<div class=\"ml-embedded\" data-form=\"UcmPfz\"></div>
+       <script src=\"https://utteranc.es/client.js\"
         repo=\"HDictus/HDictus.github.io\"
         issue-term=\"title\"
         theme=\"github-light\"

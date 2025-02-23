5845da7b97ce2d0a550076fc772c1cc39acbad6a
hugs <h.t.dictus@gmail.com>
2025-02-23 10:30:25 +0100

    update htmls to support youtube links

--- publish.el
+++ publish.el
@@ -16,6 +16,27 @@
    "<link rel=\"stylesheet\" href=\"stylesheet.css\""
 )
 
+(defvar yt-iframe-format
+  ;; You may want to change your width and height.
+  (concat "<iframe width=\"440\""
+          " height=\"335\""
+          " src=\"https://www.youtube.com/embed/%s\""
+          " frameborder=\"0\""
+          " allowfullscreen>%s</iframe>"))
+
+(org-add-link-type
+ "yt"
+ (lambda (handle)
+   (browse-url
+    (concat "https://www.youtube.com/embed/"
+            handle)))
+ (lambda (path desc backend)
+   (cl-case backend
+     (html (format yt-iframe-format
+                   path (or desc "")))
+     (latex (format "\href{%s}{%s}"
+                    path (or desc "video"))))))
+
 (setq org-html-postamble
       "<div class=\"ml-embedded\" data-form=\"UcmPfz\"></div>
        <script src=\"https://utteranc.es/client.js\"
